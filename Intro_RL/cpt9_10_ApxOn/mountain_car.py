834f780f26d1a341c8968dadae70139d49ea1f2f
zhaoyu <zhaoyu775885@163.com>
2020-08-20 22:51:36 +0800

    add IHT tile coding for Mountain-Car

--- Intro_RL/cpt9_10_ApxOn/mountain_car.py
+++ Intro_RL/cpt9_10_ApxOn/mountain_car.py
@@ -1,19 +1,25 @@
 import gym
 import time
+from IHT import tiles, IHT
 
-#导入MountainCar-v0环境
-env = gym.make('MountainCar-v0')
-#初始化环境
-env.reset()
-#循环1000次
-for _ in range(1000):
-    #绘图
-    env.render()
-    #进行一个动作
-    info = env.step(env.action_space.sample()) # take a random action
+def run():
+    #导入MountainCar-v0环境
+    env = gym.make('MountainCar-v0')
+    #初始化环境
+    env.reset()
+    #循环1000次
+    for _ in range(1000):
+        #绘图
+        env.render()
+        #进行一个动作
+        info = env.step(env.action_space.sample()) # take a random action
+        
+        print(info)
+        # 慢动作展示
+        time.sleep(0.001)
+    #关闭
+    env.close()
     
-    print(info)
-    # 慢动作展示
-    time.sleep(0.001)
-#关闭
-env.close()
+if __name__ == '__main__':
+    iht = IHT(1024)
+    tiles(iht, 8, [3.6, 7.21])
--- Intro_RL/gym/main.py
+++ Intro_RL/gym/main.py
@@ -9,13 +9,14 @@ Created on Fri Jul 26 19:43:58 2019
 import gym
 #env = gym.make('CartPole-v0')
 env = gym.make('MountainCar-v0')
-for i_episode in range(20):
+for i_episode in range(1):
     observation = env.reset()
     for step in range(100):
         env.render()
-        print(observation)
-        action = env.action_space.sample()
+        # action = env.action_space.sample()
+        action = 2
         observation, reward, done, info = env.step(action)
+        print(action, observation, reward)
         if done:
             print("Episode finished after {} timesteps".format(step+1))
             break
