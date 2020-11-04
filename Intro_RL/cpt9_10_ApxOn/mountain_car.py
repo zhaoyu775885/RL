96bc203fe59ac39f7cb1586f381b403ca02015e6
zhaoyu775885 <zhaoyu775885@163.com>
2020-11-04 18:07:36 +0800

    update SemiGrad and Q-learning for mountaincar

--- Intro_RL/cpt9_10_ApxOn/mountain_car.py
+++ Intro_RL/cpt9_10_ApxOn/mountain_car.py
@@ -20,15 +20,18 @@ def run():
     env.close()
     
 class MountainCar():
-    def __init__(self, num_tilings=8):
+    def __init__(self, 
+                 num_tilings=8, 
+                 num_tiles_one_dim=8,
+                 num_states=4096):
         self.env = gym.make('MountainCar-v0')
-        self.num_states = 4096
+        self.num_states = num_states
         self.iht = IHT(self.num_states)
         self.num_tilings = num_tilings
-        self.num_tiles_one_dim = 8
-        self.num_actions = self.env.action_space.n
+        self.num_tiles_one_dim = num_tiles_one_dim
         self.obs_ranges = [h-l for h,l in zip(self.env.observation_space.high,
                                               self.env.observation_space.low)]
+        self.num_actions = self.env.action_space.n
         self.state = self.init()
         
     def tilecoder(self, obs, act=0):
@@ -39,7 +42,7 @@ class MountainCar():
     
     def init(self):
         obs = self.env.reset()
-        self.env.render()
+        # self.env.render()
         return obs
     
     def get_state(self):
@@ -49,25 +52,30 @@ class MountainCar():
         return self.env.action_space.sample()
         
     def take_action(self, a):
-        obs, r, status, info = self.env.step(a)
-        self.env.render()
-        self.state = obs
-        return obs, r, status
+        s, r, status, info = self.env.step(a)
+        # self.env.render()
+        self.state = s
+        return s, r, status
 
     def close(self):
-        if self.env:
+        if self.env is not None:
             self.env.close()
-        self.env = None
-                
+            self.env = None
+        
+    def __del__(self):
+        print('running destructor')
+        self.close()
+
 
 class SemiGradSarsa():
     def __init__(self, env):
         self.env = env
         self.n_actions = self.env.num_actions
         self.weight = np.zeros([self.env.num_states])
-        self.epsilon = 0
+        self.epsilon = 0.1
         self.alpha = 0.5/8
         self.gamma = 0.99
+        self.debug = False
     
     def q(self, state, action):
         active_tiles = self.env.tilecoder(state, action)
@@ -89,29 +97,145 @@ class SemiGradSarsa():
         return g_action
     
     def train(self):
-        num_eposide = 1
+        num_eposide = 1000
         for _ in range(num_eposide):
+            print('Episode ', _+1, ': ', end=' ')
             s = self.env.init()
             a = self.greedy_epsilon(s)
             cnt = 0
+            act_list = [a]
             while True:
                 cnt += 1
-                s_, r, done = self.env.take_action(a)
                 tile_code = self.env.tilecoder(s, a)
+                s_, r, done = self.env.take_action(a)
+                act_list.append(a)
                 if done:
-                    print('really done?!', cnt)
+                    if cnt < 200:
+                        print('mission completed with {0} steps'.format(cnt))
+                    else:
+                        print('mission failed')
                     self.weight[tile_code] += self.alpha*(r-self.q(s, a))
                     break
                 a_ = self.greedy_epsilon(s_)
-                if (cnt+1) % 100 == 0:
-                    print(self.weight)
+                if (cnt+1) % 50 == 0 and self.debug:
+                    print(act_list)
+                    act_list.clear()
                 self.weight[tile_code] += self.alpha*(r+self.gamma*self.q(s_, a_)-self.q(s, a))
                 s, a = s_, a_
+            if (_+1) % 5 == 0:
+                self.test()
+                self.epsilon *= 0.5
+                
+    def test(self):
+        print('Policy Testing: ', end=' ')
+        s = self.env.init()
+        act_list = []
+        cnt = 0
+        while True:
+            cnt += 1
+            a = self.greedy(s)
+            act_list.append(a)
+            s_, r, done = self.env.take_action(a)
+            s = s_
+            if done:
+                if cnt < 200:
+                    print('mission completed with {0} steps'.format(cnt))
+                else:
+                    print('mission failed')
+                break
+            
 
+class QLearning():
+    def __init__(self, env):
+        self.env = env
+        self.n_actions = self.env.num_actions
+        self.q_tab = np.zeros([self.env.num_states])
+        self.epsilon = 0.05
+        self.alpha = 0.5
+        self.gamma = 0.99
+        self.debug = False
     
+    def q(self, state, action):
+        active_tiles = self.env.tilecoder(state, action)
+        return self.q_tab[active_tiles]
+    
+    def greedy_epsilon(self, state):
+        rand = np.random.rand()
+        if rand <= self.epsilon:
+            return self.env.random_action()
+        return self.greedy(state)
+    
+    def greedy(self, state):
+        g_action = 0
+        g_val = self.q(state, g_action)
+        for action in range(1, self.n_actions):
+            q_val = self.q(state, action)
+            if q_val>g_val:
+                g_val, g_action = q_val, action
+        return g_action
+
+    def train(self, num_eposide=1000):
+        for _ in range(num_eposide):
+            print('Episode ', _+1, ': ', end=' ')
+            s = self.env.init()
+            
+            cnt = 0
+            act_list = []
+            while True:
+                cnt += 1
+                a = self.greedy_epsilon(s)
+                act_list.append(a)
+                tile_code = self.env.tilecoder(s, a)[0]
+                s_, r, done = self.env.take_action(a)
+                a_max = self.greedy(s_)
+                if (cnt+1) % 50 == 0 and self.debug:
+                    print(act_list)
+                    act_list.clear()
+                self.q_tab[tile_code] += self.alpha*(r+self.gamma*self.q(s_, a_max)-self.q_tab[tile_code])
+                s = s_
+                
+                if done:
+                    if cnt < 200:
+                        print('mission completed with {0} steps'.format(cnt))
+                    else:
+                        print('mission failed')
+                    break
+                
+            if (_+1) % 100 == 0:
+                self.test()
+                self.epsilon *= 0.9
+                self.alpha *= 0.9
+                
+    def test(self):
+        print('Policy Testing: ', end=' ')
+        s = self.env.init()
+        act_list = []
+        cnt = 0
+        while True:
+            cnt += 1
+            a = self.greedy(s)
+            act_list.append(a)
+            s_, r, done = self.env.take_action(a)
+            s = s_
+            if done:
+                if cnt < 200:
+                    print('mission completed with {0} steps'.format(cnt))
+                else:
+                    print('mission failed')
+                break
+            
+
 if __name__ == '__main__':
     # run()
-    env = MountainCar()
+    
+    # Algo 1: Semi-Gradient Sarsa
+    env = MountainCar(num_tilings=8, num_tiles_one_dim=8, num_states=4096)
     leaner = SemiGradSarsa(env)
     leaner.train()
     env.close()
+    
+    # # Algo 2: 
+    # env = MountainCar(num_tilings=1, num_tiles_one_dim=32, num_states=10000)
+    # leaner = QLearning(env)
+    # leaner.train(num_eposide=10000)
+    # env.close()
